f9ce490437208d92f13d9a2643d1c54aa38dd238
Brayan Vinicius Jordan <brayan.11fera@gmail.com>
2023-10-11 19:26:53 -0300

    test: Refatorando testes E2E com o uso de factories para maior reutilização de código

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common'
 import { PrismaService } from './prisma/prisma.service'
 import { PrismaAnswerAttachmentsRepository } from './prisma/repositories/prisma-answer-attachments-repository'
 import { PrismaAnswersRepository } from './prisma/repositories/prisma-answers-repository'
-import { PrismaAnwerCommentsRepository } from './prisma/repositories/prisma-answer-comments-repository'
+import { PrismaAnswerCommentsRepository } from './prisma/repositories/prisma-answer-comments-repository'
 import { PrismaQuestionAttachmentsRepository } from './prisma/repositories/prisma-question-attachments-repository'
 import { PrismaQuestionCommentsRepository } from './prisma/repositories/prisma-question-comments-repository'
 import { PrismaQuestionsRepository } from './prisma/repositories/prisma-questions-repository'
@@ -25,14 +25,14 @@ import { PrismaStudentsRepository } from './prisma/repositories/prisma-students-
     PrismaQuestionAttachmentsRepository,
     PrismaAnswerAttachmentsRepository,
     PrismaAnswersRepository,
-    PrismaAnwerCommentsRepository,
+    PrismaAnswerCommentsRepository,
   ],
   exports: [
     PrismaService,
     QuestionsRepository,
     PrismaAnswerAttachmentsRepository,
     PrismaAnswersRepository,
-    PrismaAnwerCommentsRepository,
+    PrismaAnswerCommentsRepository,
     PrismaQuestionAttachmentsRepository,
     PrismaQuestionCommentsRepository,
     StudentsRepository,
