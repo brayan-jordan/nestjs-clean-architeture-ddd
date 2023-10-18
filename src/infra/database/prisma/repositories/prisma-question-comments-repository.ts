ffbbfb24e72ec90a3477900babafe516a9f3418e
Brayan Vinicius Jordan <brayan.11fera@gmail.com>
2023-10-18 19:26:56 -0300

    feat: Iniciando com o uso de value-objects para facilitar o envio de informações de entidades com seus relacionamentos entre todas as camadas da aplicação.

--- src/infra/database/prisma/repositories/prisma-question-comments-repository.ts
+++ src/infra/database/prisma/repositories/prisma-question-comments-repository.ts
@@ -4,6 +4,8 @@ import { QuestionComment } from '@/domain/forum/enterprise/entities/question-com
 import { Injectable } from '@nestjs/common'
 import { PrismaQuestionCommentMapper } from '../mappers/prisma-question-comment-mapper'
 import { PrismaService } from '../prisma.service'
+import { CommentWithAuthor } from '@/domain/forum/enterprise/entities/value-objects/comment-with-author'
+import { PrismaCommentWithAuthorMapper } from '../mappers/prisma-comment-with-author-mapper'
 
 @Injectable()
 export class PrismaQuestionCommentsRepository
@@ -49,5 +51,26 @@ export class PrismaQuestionCommentsRepository
     })
 
     return questionComments.map(PrismaQuestionCommentMapper.toDomain)
+  }
+
+  async findManyByQuestionIdWithAuthor(
+    questionId: string,
+    { page }: PaginationParams,
+  ): Promise<CommentWithAuthor[]> {
+    const questionComments = await this.prisma.comment.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      },
+      skip: (page - 1) * 20,
+      take: 20,
+      where: {
+        questionId,
+      },
+      include: {
+        author: true,
+      },
+    })
+
+    return questionComments.map(PrismaCommentWithAuthorMapper.toDomain)
   }
 }
